c5faab4e8348bdc19becfa92dbd6774e731287b7
aminSimyari <149312884+aminSimyari@users.noreply.github.com>
2026-08-26 20:41:09 +0330

    add yaml dep

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module '*.yml' {
+  const content: Record<string, unknown>
+  export default content
+}
+
+declare module '*.yaml' {
+  const content: Record<string, unknown>
+  export default content
+}
