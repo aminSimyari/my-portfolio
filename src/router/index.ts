0f6e5201db096d9fdb3141bb219755f4b2ebdd3c
aminSimyari <149312884+aminSimyari@users.noreply.github.com>
2026-08-29 18:57:44 +0330

    edit for router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'home',
+      component: Home,
+    },
+  ],
+  scrollBehavior() {
+    return { top: 0 }
+  },
 })
 
 export default router
